9de211e817f6d3acfa87a329f3d22b03120e064e
Romain Bach <roalexba83@gmail.com>
2022-10-28 09:17:59 +0200

    first

--- src/UDPserver.java
+++ src/UDPserver.java
@@ -2,4 +2,12 @@ public class UDPserver {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+    // attribut
+    protected int port;
+    protected int port2;
+
+    // constructor
+    UDPserver(int port){
+        this.port = port;
+    }
 }
