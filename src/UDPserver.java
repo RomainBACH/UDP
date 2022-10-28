555bea678f2e4b3abd361f03fb6c5a79406f7dae
Romain Bach <roalexba83@gmail.com>
2022-10-28 09:27:57 +0200

    2nde

--- src/UDPserver.java
+++ src/UDPserver.java
@@ -1,13 +1,20 @@
 public class UDPserver {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Here is UDP server!");
+        UDPserver udpServer = new UDPserver(8080);
+        UDPserver udpServer2 = new UDPserver();
+
     }
     // attribut
-    protected int port;
-    protected int port2;
+    protected int listeningPort;
 
     // constructor
-    UDPserver(int port){
-        this.port = port;
+    UDPserver(int listeningPort){
+        this.listeningPort = listeningPort;
+    }
+
+    // constructor (default)
+    UDPserver(){
+        this.listeningPort = 32768;
     }
 }
